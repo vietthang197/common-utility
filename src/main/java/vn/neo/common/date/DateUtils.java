d699ed483536efd9fa00f7d68079894e2b074f6a
acer <thanglv@neo.vn>
2021-12-07 14:21:36 +0700

    Initial commit

---
+++ src/main/java/vn/neo/common/date/DateUtils.java
@@ -0,0 +1,34 @@
+package vn.neo.common.date;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+/**
+ * @author acer on 12/7/2021
+ * @project common-utility
+ */
+public class DateUtils {
+    public static String toString(LocalDateTime dateTime, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return formatter.format(dateTime);
+    }
+
+    public static String toString(Date date, String format) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(dateFormat);
+    }
+
+    public static LocalDateTime fromStringV2(String date, String format) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return LocalDateTime.parse(date, formatter);
+    }
+
+    public static Date fromString(String date, String format) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.parse(date);
+    }
+}
---
+++ src/main/java/vn/neo/common/http/HttpClientPool.java
@@ -0,0 +1,164 @@
+package vn.neo.common.http;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ndSenses
+ * Date: 6/2/2021
+ * Time: 4:34 PM
+ * Project: adapter-vasp
+ **/
+public class HttpClientPool {
+    private static final PoolingHttpClientConnectionManager POOLING_CONN_MANAGER = new PoolingHttpClientConnectionManager();
+    private static final CloseableHttpClient client = HttpClients
+            .custom().setConnectionManager(POOLING_CONN_MANAGER)
+            .build();
+
+    public static String sendPostSOAP(String request, String urlApi, int timeout) throws IOException {
+        HttpPost httpPost = new HttpPost(urlApi);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(timeout)
+                .setConnectTimeout(timeout)
+                .setSocketTimeout(timeout)
+                .build();
+
+        StringEntity stringEntity = new StringEntity(request, StandardCharsets.UTF_8);
+        httpPost.setConfig(requestConfig);
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "text/xml");
+        httpPost.setHeader("SOAPAction", "");
+        httpPost.setEntity(stringEntity);
+        StringBuilder response = new StringBuilder();
+        InputStreamReader isr = null;
+        BufferedReader in = null;
+        try (
+                CloseableHttpResponse httpResponse = client.execute(httpPost);
+        ) {
+            InputStream inputStream = httpResponse.getEntity().getContent();
+            isr = new InputStreamReader(inputStream);
+            in = new BufferedReader(isr);
+            String value = null;
+            while ((value = in.readLine()) != null) {
+                response.append(value);
+            }
+
+            in.close();
+            return response.toString();
+        } finally {
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException ignored) {
+                }
+            }
+
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+
+
+    public static String sendPost(String request, String urlApi, int timeout, String contentType) throws IOException {
+        HttpPost httpPost = new HttpPost(urlApi);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(timeout)
+                .setConnectTimeout(timeout)
+                .setSocketTimeout(timeout)
+                .build();
+
+        StringEntity stringEntity = new StringEntity(request, StandardCharsets.UTF_8);
+        httpPost.setConfig(requestConfig);
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, contentType);
+        httpPost.setEntity(stringEntity);
+        StringBuilder response = new StringBuilder();
+        InputStreamReader isr = null;
+        BufferedReader in = null;
+        OutputStream out = null;
+        try (
+                CloseableHttpResponse httpResponse = client.execute(httpPost);
+        ) {
+            InputStream inputStream = httpResponse.getEntity().getContent();
+            isr = new InputStreamReader(inputStream);
+            in = new BufferedReader(isr);
+            String value = null;
+            while ((value = in.readLine()) != null) {
+                response.append(value);
+            }
+
+            in.close();
+            return response.toString();
+        } finally {
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException ignored) {
+                }
+            }
+
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+
+    public static String sendGet(String request, String urlApi, int timeout, String contentType) throws IOException {
+        HttpGet httpGet = new HttpGet(urlApi);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(timeout)
+                .setConnectTimeout(timeout)
+                .setSocketTimeout(timeout)
+                .build();
+
+        httpGet.setConfig(requestConfig);
+        httpGet.setHeader(HttpHeaders.CONTENT_TYPE, contentType);
+        StringBuilder response = new StringBuilder();
+        InputStreamReader isr = null;
+        BufferedReader in = null;
+        try (
+                CloseableHttpResponse httpResponse = client.execute(httpGet);
+        ) {
+            InputStream inputStream = httpResponse.getEntity().getContent();
+            isr = new InputStreamReader(inputStream);
+            in = new BufferedReader(isr);
+            String value = null;
+            while ((value = in.readLine()) != null) {
+                response.append(value);
+            }
+
+            in.close();
+            return response.toString();
+        } finally {
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException ignored) {
+                }
+            }
+
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}
